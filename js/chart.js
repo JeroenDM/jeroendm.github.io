575d999068d6239f28c503d9c463b794f23aa30d
JeroenDM <jeroendemaeyer@live.be>
2016-03-30 17:22:25 +0200

    add plotMudole

--- js/chart.js
+++ js/chart.js
@@ -1,37 +1,36 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-  width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+var plotModule = (function () {
 
-var x = d3.scale.linear()
-    .range([0, width]);
+  // Setup plot window
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+  var x = d3.scale.linear().range([0, width]),
+      y = d3.scale.linear().range([height, 0]),
+      xAxis = d3.svg.axis().scale(x).orient("bottom"),
+      yAxis = d3.svg.axis().scale(y).orient("left");
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+  var line = d3.svg.line()
+    .x(function(d) { return x(d.tijd); })
+    .y(function(d) { return y(d.snelheid); })
+    .interpolate("basis");
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var line = d3.svg.line()
-  .x(function(d) { return x(d.tijd); })
-  .y(function(d) { return y(d.snelheid); })
-  .interpolate("basis");
+  // Public functions
+  function initPlot() {
+    updatePlot(0, [{tijd: 0, snelheid: 0}]);
+  }
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.tsv("/data/test1.txt", type, function(error, data) {
+  function updatePlot(error, data) {
     if (error) throw error;
 
-    x.domain(d3.extent(data, function(d) { return d.tijd; }));
-    y.domain(d3.extent(data, function(d) { return d.snelheid; }));
+    x.domain(d3.extent(data, function(d) { return +d.tijd; }));
+    y.domain(d3.extent(data, function(d) { return +d.snelheid; }));
 
     svg.append("g")
         .attr("class", "axis")
@@ -44,18 +43,22 @@ d3.tsv("/data/test1.txt", type, function(error, data) {
       .append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Snelheid (km/u)");
+        .attr("dy", ".71em");
+        //.style("text-anchor", "end")
+        //.text("Snelheid (km/u)");
 
     svg.append("path")
         .datum(data)
         .attr("class", "line")
         .attr("d", line);
-  });
-
-  function type(d) {
-    d.tijd= Number(d.tijd);
-    d.snelheid = Number(d.snelheid);
-    return d;
   }
+
+  // Reveal public vars and funcs
+  return {
+    init: initPlot,
+    update: updatePlot
+  }
+})();
+
+plotModule.init();
+d3.tsv("/data/test1.txt", plotModule.update);
