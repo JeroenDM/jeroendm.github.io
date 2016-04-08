08ae1df8d3ce67d34c096412be4a1f59867e7b91
JeroenDM <jeroendemaeyer@live.be>
2016-04-08 16:04:22 +0200

    move all module definition to one file

--- js/modules.js
+++ js/modules.js
@@ -79,6 +79,53 @@ var plotModule = (function () {
   }
 })();
 
-plotModule.create();
+var dataModule = (function() {
+  // initialize data acces
+
+  d3.tsv(path + fileNames[0], plotModule.update);
+
+  function updatePlot() {
+      var name = document.getElementById("fileSelection").value;
+      d3.tsv(path + name, plotModule.update);
+  }
+
+  return {
+    update: updatePlot
+  }
+})();
+
+var formModule = (function() {
+
+  // Init file selection menu
+  var $select = d3.select("#fileSelection");
+  for (i=0; i < fileNames.length; i++) {
+    $select.append("option")
+            .attr("value", fileNames[i])
+            .text(fileNames[i]);
+  }
+
+  var header = ["koppel", "stroom"];
+  var labels = ["Torque", "Current"];
+  var $div = d3.select("#yVarSelection");
+
+  // some realy ugly dom handling
+  function updateYVars() {
+    for (i=0; i<labels.length; i++) {
+      var $label = $div.append("div")
+                    .attr("class", "radio")
+                    .append("label")
+      $label.append("input")
+            .attr("type", "radio")
+            .attr("name", "yVarSelection")
+            .attr("value", header[i])
+      $label.append("p").text(labels[i]);
+    }
+
+  }
+
+  return {
+    updateYVars: updateYVars
+  }
+})();
 
 //var test3 = [{tijd: 0, snelheid: 0}, {tijd: 1, snelheid: 2}, {tijd: 2, snelheid: 1}];
