31dfbf17d1e948e899a8641c55704d2c063e3c60
JeroenDM <jeroendemaeyer@live.be>
2016-07-13 12:11:01 +0200

    veel veranderingen aan maat.html en .js, tijd voor eten nu

--- tgvelolabel/js/maat.js
+++ tgvelolabel/js/maat.js
@@ -1,23 +1,65 @@
-function calcSize() {
-    var Lzmax = parseFloat(document.getElementById("Lzmax").value);
-    var Lzmin = parseFloat(document.getElementById("Lzmin").value);
+// module to manage bike sizing calculation
+var sizeModule = (function () {
     
-    document.getElementById("maxmaxLengte").innerHTML = Math.round(Lzmax / 45 / 1.1 * 10000) / 100 + " m";
-    document.getElementById("maxLengte").innerHTML = Math.round(Lzmax / 48 / 1.1 * 10000) / 100 + " m";
-    document.getElementById("minLengte").innerHTML = Math.round(Lzmin / 45 / 1.1 * 10000) / 100 + " m";
-    document.getElementById("minminLengte").innerHTML = Math.round(Lzmin / 48 / 1.1 * 10000) / 100 + " m";
+    var hscale = d3.scale.linear().domain([0, 90]).range([0, 188]);
     
-    var h1 = 50;
-    var h2 = 150;
-    var h3 = 120;
-    var h4 = 50;
+    function calcSize() {
+        var Lzmax = parseFloat( document.getElementById("Lzmax").value );
+        var Lzmin = parseFloat( document.getElementById("Lzmin").value );
+
+        if (Lzmin < 0 || Lzmin > 2) {
+            alert("Lz laag moet tussen 0 en 2 meter zijn.")
+        } else if (Lzmax < 0 || Lzmax > 2) {
+            alert("Lz hoog moet tussen 0 en 2 meter zijn.")
+        } else {
+            document.getElementById("maxmaxLengte").innerHTML = Math.round(Lzmax / 45 / 1.1 * 10000) / 100 + " m";
+            document.getElementById("maxLengte").innerHTML = Math.round(Lzmax / 48 / 1.1 * 10000) / 100 + " m";
+            document.getElementById("minLengte").innerHTML = Math.round(Lzmin / 45 / 1.1 * 10000) / 100 + " m";
+            document.getElementById("minminLengte").innerHTML = Math.round(Lzmin / 48 / 1.1 * 10000) / 100 + " m";
+        }
+    }
+    
+    function calcPost() {
+        alert("the button is working!");
+    
+        var h1 = hscale(0);
+        var h2 = hscale(30);
+        var h3 = hscale(60);
+        var h4 = hscale(90);
+        d3.select(".arrow1").attr("transform", "translate(" + h1 + ",0)");
+        d3.select(".arrow1").attr("transform", "translate(" + h2 + ",0)");
+        d3.select(".arrow1").attr("transform", "translate(" + h3 + ",0)");
+        d3.select(".arrow4").attr("transform", "translate(" + h4 + ",0)");
+    }
+    
+    return {
+        calcSize: calcSize,
+        calcPost: calcPost
+    };
+})();
+
+
+
+/*
+
+var Lsmin = parseFloat( document.getElementById("Lsmin").value );
+    var Lsmax = parseFloat( document.getElementById("Lsmax").value );
+    var Hzmin = parseFloat( document.getElementById("Hzmin").value );
+    var Hzmax = parseFloat( document.getElementById("Hzmax").value );
+    var Hsmin = parseFloat( document.getElementById("Hsmin").value );
+    var Hsmax = parseFloat( document.getElementById("Hsmax").value );
+    
+    var hscale = d3.scale.linear().domain([0, 90]).range([4, 204]);
+    
+    var h1 = hscale(0);
+    var h2 = hscale(30);
+    var h3 = hscale(60);
+    var h4 = hscale(90);
     d3.select(".arrow1").attr("transform", "translate(" + h1 + ",0)");
     d3.select(".arrow1").attr("transform", "translate(" + h2 + ",0)");
     d3.select(".arrow1").attr("transform", "translate(" + h3 + ",0)");
     d3.select(".arrow4").attr("transform", "translate(" + h4 + ",0)");
-}
-
-/*
+    
 const UNIT = 80;
 const WIDTH = 6 * UNIT;
 const HEIGHT = 6 * UNIT;
