027f4fad2b7c230c7ea75e375d6fd9cce2b1d9dc
JeroenDM <jeroendemaeyer@live.be>
2016-07-13 14:54:20 +0200

    compleet nieuwe strategie om maatsysteem app te maken

--- tgvelolabel/js/maat.js
+++ tgvelolabel/js/maat.js
@@ -1,7 +1,28 @@
 // module to manage bike sizing calculation
 var sizeModule = (function () {
     
-    var hscale = d3.scale.linear().domain([0, 90]).range([0, 188]);
+    var hscale = d3.scale.linear().domain([90, 45]).range([0, 188]);
+    var len = {
+        maxmax: 0,
+        max: 0,
+        min: 0,
+        minmin: 0
+    };
+    var dat = {
+        Hs: 0,
+        Hzmin: 0,
+        Hzmax: 0,
+        Lsmin: 0,
+        Lsmax: 0
+    };
+    
+    function _parseValues() {
+        dat.Hs = parseFloat( document.getElementById("Hs").value );
+        dat.Hzmin = parseFloat( document.getElementById("Hzmin").value );
+        dat.Hzmax = parseFloat( document.getElementById("Hzmax").value );
+        dat.Lsmin = parseFloat( document.getElementById("Lsmin").value );
+        dat.Lsmax = parseFloat( document.getElementById("Lsmax").value );
+    }
     
     function calcSize() {
         var Lzmax = parseFloat( document.getElementById("Lzmax").value );
@@ -12,33 +33,65 @@ var sizeModule = (function () {
         } else if (Lzmax < 0 || Lzmax > 2) {
             alert("Lz hoog moet tussen 0 en 2 meter zijn.")
         } else {
-            document.getElementById("maxmaxLengte").innerHTML = Math.round(Lzmax / 45 / 1.1 * 10000) / 100 + " m";
-            document.getElementById("maxLengte").innerHTML = Math.round(Lzmax / 48 / 1.1 * 10000) / 100 + " m";
-            document.getElementById("minLengte").innerHTML = Math.round(Lzmin / 45 / 1.1 * 10000) / 100 + " m";
-            document.getElementById("minminLengte").innerHTML = Math.round(Lzmin / 48 / 1.1 * 10000) / 100 + " m";
+            len.maxmax = Math.round(Lzmax / 45 / 1.1 * 10000) / 100;
+            len.max = Math.round(Lzmax / 48 / 1.1 * 10000) / 100;
+            len.min = Math.round(Lzmin / 45 / 1.1 * 10000) / 100;
+            len.minmin = Math.round(Lzmin / 48 / 1.1 * 10000) / 100;
+            
+            document.getElementById("maxmaxLengte").innerHTML = len.maxmax + " m";
+            document.getElementById("maxLengte").innerHTML = len.max + " m";
+            document.getElementById("minLengte").innerHTML = len.min + " m";
+            document.getElementById("minminLengte").innerHTML = len.minmin + " m";
         }
     }
     
     function calcPost() {
-        alert("the button is working!");
-    
-        var h1 = hscale(0);
-        var h2 = hscale(30);
-        var h3 = hscale(60);
-        var h4 = hscale(90);
-        d3.select(".arrow1").attr("transform", "translate(" + h1 + ",0)");
-        d3.select(".arrow1").attr("transform", "translate(" + h2 + ",0)");
-        d3.select(".arrow1").attr("transform", "translate(" + h3 + ",0)");
-        d3.select(".arrow4").attr("transform", "translate(" + h4 + ",0)");
+        // get input values
+        //------------------------------------------------------------------
+        _parseValues();
+        
+        // zadel laag
+        //------------------------------------------------------------------
+        var lmin = (len.min + len.minmin) / 2;
+        var Hsz = dat.Hs - dat.Hzmin - 0.095*lmin;
+        var ld = Math.sqrt(dat.Lsmin * dat.Lsmin + Hsz * Hsz);
+        
+        var a = Math.asin( Hsz / ld );
+        
+        var b = Math.acos( ((0.26 * lmin)*(0.26 * lmin) + ld*ld - (0.42*lmin)*(0.42*lmin)) / (2*0.26*lmin*ld) );
+        
+        var gammaL = (a+b) * 180 / Math.PI;
+        console.log("gamma laag: " + gammaL);
+        if (gammaL > 90) {gammaL = 90};
+        
+        d3.select(".arrow1").attr("transform", "translate(" + hscale(gammaL) + ",0)");
+        
+        // zadel hoog
+        //------------------------------------------------------------------
+        var lmax = (len.max + len.maxmax) / 2;
+        var Hsz = dat.Hs - dat.Hzmax - 0.095*lmax;
+        var ld = Math.sqrt(dat.Lsmax * dat.Lsmax + Hsz * Hsz);
+        
+        var a = Math.asin( Hsz / ld );
+        
+        var b = Math.acos( ((0.26 * lmax)*(0.26 * lmax) + ld*ld - (0.42*lmax)*(0.42*lmax)) / (2*0.26*lmax*ld) );
+        
+        var gammaH = (a+b) * 180 / Math.PI;
+        console.log("gamma hoog: " + gammaH);
+        if (gammaH > 90) {gammaH = 90};
+        
+        d3.select(".arrow2").attr("transform", "translate(" + hscale(gammaH) + ",0)");
     }
     
     return {
         calcSize: calcSize,
-        calcPost: calcPost
+        calcPost: calcPost,
+        dat: dat,
+        len: len
     };
 })();
 
-
+sizeModule.calcSize();
 
 /*
 
